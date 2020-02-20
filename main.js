37c62cf74853cdc74c9375812d1d4ac2ed8f09ce
umar <umar.baba@softwareag.com>
2020-02-20 18:33:22 +0530

    added new chabegs

---
+++ main.js
@@ -0,0 +1,126 @@
+const { app, BrowserWindow, Menu,Tray } = require('electron')
+const electron =require('electron')
+const path = require('path');
+
+var mainWindow
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  mainWindow.loadFile('index.html')
+  mainWindow.setThumbarButtons([
+
+    {
+      tooltip: 'connect',
+      icon:  electron.nativeImage.createFromPath(path.join(__dirname, 'connect.jpg')),
+      click() { console.log('button1 clicked') }
+    },
+    {
+      tooltip: 'connect',
+      icon:  electron.nativeImage.createFromPath(path.join(__dirname, 'connect2.png')),
+      click() { console.log('button1 clicked') }
+    }
+  ])
+  mainWindow.on('closed', () => {
+    app.quit()
+  })
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+    mainWindow.focus()
+  })
+  mainWindow.maximize()
+  
+ 
+  //mainWindow.webContents.openDevTools()
+}
+
+function createMenu() {
+  const menu = Menu.buildFromTemplate(menuItemsTemplate)
+  Menu.setApplicationMenu(menu)
+}
+
+function CreateHostAddWindow(){
+  const connectWin = new BrowserWindow({
+    width: 400,
+    height: 400,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  connectWin.loadFile('src/addHost.html')
+  connectWin.setTitle("Add Host");
+  connectWin.removeMenu()
+}
+
+function createConnectWindow() {
+  const connectWin = new BrowserWindow({
+    width: 700,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  connectWin.loadFile('src/connect.html')
+  connectWin.setTitle("Connect");
+  const connectMenuTemplate = [
+    {
+      label:"Create",
+      click(){
+        CreateHostAddWindow()
+      }
+    },
+    {
+      label:"Remove"
+    }
+  ]
+  const menu = Menu.buildFromTemplate(connectMenuTemplate)
+  connectWin.setMenu(menu)
+
+
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  createMenu()
+})
+
+
+
+app.on('window-all-closed', () => {
+
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+menuItemsTemplate = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: 'Connect',
+        click() {
+          createConnectWindow()
+        }
+      },
+      {
+        label: 'Quit',
+        accelerator: process.platform == 'darwin' ? 'command+Q' : 'ctrl+Q',
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+]
